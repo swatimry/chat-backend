98ffcf5e3f6940aedd878f19f8db3052aa88ff65
Swati <swati2003mry@gmail.com>
2023-10-07 21:02:48 +0530

    links update 3

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 const server=http.createServer(app); 
 const io=socketio(server,{
     cors:{
-        origin: [process.env.FRONTEND_URL,"http://localhost:3000"],
+        origin: "https://darling-valkyrie-1752de.netlify.app",
         methods:["GET","POST"],
         allowedHeaders: ["my-custom-header"],
     credentials: true
